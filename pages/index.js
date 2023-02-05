8e25f8ce67a362c7d9aced686afeaae15acb28a9
ShakeelF <sfatdev@gmail.com>
2023-02-05 03:18:43 +0100

    setup typescript

--- pages/index.js
+++
@@ -1,5 +0,0 @@
-function HomePage() {
-    return <div className="text-3xl font-bold underline">My Games Library <button className="btn">Hello daisyUI</button> </div>;
-  }
-  
-export default HomePage
---
+++ pages/index.tsx
@@ -0,0 +1,8 @@
+function HomePage() {
+    return <div className="text-3xl font-bold underline">
+      My Games Library 
+      <button className="btn">Hello daisyUI</button> 
+    </div>;
+  }
+  
+export default HomePage
