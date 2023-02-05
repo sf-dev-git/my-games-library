dcd3e6a2ee33f9484bc29c3e1b6097027c4fc7f5
ShakeelF <sfatdev@gmail.com>
2023-02-05 19:54:14 +0100

    create footer

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,18 @@
 import { AppProps } from 'next/app'
 import Layout from '../src/components/Layout'
 import '../styles/global.css'
-import Navbar from "../src/components/Navbar";
+import Navbar from '../src/components/Navbar';
+import Footer from '../src/components/Footer';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return(
-      <Layout>
-        <Navbar/>
-        <Component {...pageProps} />
-      </Layout>
+      <>
+          <Navbar/>
+          <Layout>
+            <Component {...pageProps} />
+          </Layout>
+          <Footer/>
+      </>
   );
 }
 
